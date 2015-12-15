1a79ec733793a9d0e80b2d8e20ac456621afcd5a
Aayush Naik <aayushnaik17@gmail.com>
2015-12-15 20:08:36 +0300

    Build simple calculator

--- Zipline_Calculator/js/calc.js
+++ Zipline_Calculator/js/calc.js
@@ -1,7 +1,65 @@
+expStack = [];
+operators = ['+', '-', '/', '*'];
+
+function evaluate(curr) {
+    if(curr !== "") {
+        expStack.push(curr);
+        evString = expStack.join(" ");
+        ans = eval(evString);
+        expStack = expStack.splice(0, 0);
+        //expStack.push(ans.toString());
+        return ans;
+    }
+    return "";
+}
+
 $(document).ready(function() {
     $(".my-btn").click(function() {
-        var curScreenText = $("#screen-text").html();
-        curScreenText += $(this)
+        $(this).animate({top: '+=3px'}, {duration: 50});
+        $(this).animate({top: '-=3px'}, {duration: 50});
     });
 
+    $(".num").click(function() {
+        myNum = $(this).children("p").html();
+        var val = $("#screen-text").html();
+        val += myNum;
+        $("#screen-text").html(val);
+    });
+
+    $("#dec").click(function() {
+       var val = $("#screen-text").html();
+       if(val.indexOf('.') === -1) {
+        val += '.';
+        $("#screen-text").html(val);
+       } 
+    });
+
+    $(".bi-operator").click(function() {
+        if(operators.indexOf(expStack[expStack.length - 1]) !== -1)
+            expStack.pop();
+        var val = $("#screen-text").html();
+        expStack.push(val);
+        var operator = $(this).children("p").html();
+        if(operator === 'x')
+            operator = '*';
+        if(operator === "\u00F7")
+            operator = '/';
+        expStack.push(operator);
+        //console.log(expStack);
+        $("#screen-text").html("");
+    });
+
+    $("#equals").click(function() {
+        var val = $("#screen-text").html();
+        $("#screen-text").html(evaluate(val));
+    });
+
+    $("#ce").click(function() {
+        $("#screen-text").html('');
+    });
+
+    $("#ac").click(function() {
+        expStack = expStack.splice(0, 0);
+        $("#screen-text").html('');
+    });
 });
